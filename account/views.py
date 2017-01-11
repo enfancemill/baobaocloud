346db1f4b2ecd7d89089aebc6aa1f723875be092
245967906 <245967906@qq.com>
2017-01-10 21:44:12 -0500

    登录接口/account/login/: 新增页面跳转逻辑
    1. 优化登录接口视图函数代码, 新增登录成功后的页面跳转逻辑
    2. 修改登录接口html模板以支持页面跳转
    3. 修改URLconf, 为首页接口传递name参数
    
    modified:   account/templates/login.html
    modified:   account/views.py
    modified:   baobaocloud/urls.py

--- account/views.py
+++ account/views.py
@@ -1,21 +1,29 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render
 from django.contrib import auth
+from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
+    interface = request.get_full_path()
     if request.method == 'GET':
-        return render(request, 'login.html', {'is_authentication_required': False})
+        is_auth_failed = False
+        next_page = request.GET.get('next', reverse('index'))
+        context = {'is_auth_failed': is_auth_failed, 'next_page': next_page, 'interface': interface}
+        return render(request, 'login.html', context)
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        next_page = request.POST.get('next', reverse('index'))
         user = auth.authenticate(username=username, password=password)
         if user:
             auth.login(request, user)
-            return render(request, 'login.html', {'is_authentication_required': True, 'is_authentication_succeed': True})
+            return HttpResponseRedirect(next_page)
         else:
-            return render(request, 'login.html', {'is_authentication_required': True, 'is_authentication_succeed': False})
+            is_auth_failed = True
+            context = {'is_auth_failed': is_auth_failed, 'next_page': next_page, 'interface': interface}
+            return render(request, 'login.html', context)
     else:
         return HttpResponseBadRequest()
--- baobaocloud/urls.py
+++ baobaocloud/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
     url(r'^account/', include('account.urls')),
+    url(r'^$', views.index, name='index'),
 ]
