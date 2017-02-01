559614ce51aabbc0daa8f9a329f17be52303dc86
245967906 <245967906@qq.com>
2017-02-01 19:54:49 +0800

    新增注册接口: /account/register/
    1. 新增注册接口URLconf
    2. 新增注册接口视图函数
    3. 新增注册页面html模板
    4. 修改常量定义模块, 新增用户名已存在消息代码
    5. 优化登录页面html模板, 新增placeholder属性
    6. 优化登录/注册接口函数逻辑, 实现登录/注册后跳转至Referer页面
    
    修改：     account/urls.py
    修改：     account/views.py
    修改：     baobaocloud/templates/login.html
    新文件：   baobaocloud/templates/register.html
    修改：     baobaocloud/utils/const.py

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db.utils import IntegrityError
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,12 +13,13 @@ from baobaocloud.utils.shortcuts import get_msg_code
 
 
 @nologin_required()
-@method_required('GET','POST')
+@method_required('GET', 'POST')
 def login(request):
     if request.method == 'GET':
         error_msg = None
         interface = request.get_full_path()
-        next_page = request.GET.get('next', reverse('index'))
+        referer = urlparse(request.META.get('HTTP_REFERER', reverse('index')))
+        next_page = request.GET.get('next', referer.path)
         context = dict(error_msg=error_msg, interface=interface, next_page=next_page)
         return render(request, 'login.html', context)
     else:
@@ -45,3 +47,33 @@ def logout(request):
     next_page = referer.path
     auth.logout(request)
     return HttpResponseRedirect(next_page)
+
+@nologin_required()
+@method_required('GET', 'POST')
+def register(request):
+    if request.method == 'GET':
+        error_msg = None
+        interface = request.get_full_path()
+        referer = urlparse(request.META.get('HTTP_REFERER', reverse('index')))
+        next_page = request.GET.get('next', referer.path)
+        context = dict(error_msg=error_msg, interface=interface, next_page=next_page)
+        return render(request, 'register.html', context)
+    else:
+        interface = request.get_full_path()
+        next_page = request.POST.get('next', reverse('index'))
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        verify_code = request.POST.get('verify_code', '')
+        if request.session['verify_code'] == verify_code.lower():
+            try:
+                user = User.objects.create_user(username, email, password)
+            except IntegrityError:
+                error_msg = get_msg_code(4)
+            else:
+                auth.login(request, user)
+                return HttpResponseRedirect(next_page)
+        else:
+            error_msg = get_msg_code(3)
+        context = dict(error_msg=error_msg, interface=interface, next_page=next_page)
+        return render(request, 'register.html', context)
